f10c0d29eb6ebeab136df3694b3c6f7af3238156
reyvknd <reyvkianodirangga@gmail.com>
2024-10-26 10:37:01 +0700

    finix

---
+++ settings.js
@@ -0,0 +1,12 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['6285727819741'] //OWNER
+global.botname = '𝐅𝐈𝐍𝐈𝐗 𝐅𝐈𝐕𝐄🦖' //BOT NAME
+global.versisc = "5.0.0" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "Tama_Ultraa" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "Tama_Ultraa" //OPSIONAL
+global.simbol = "" // SIMBOL MENU
+global.idCH = "0012345678998765432100@newsletter"
